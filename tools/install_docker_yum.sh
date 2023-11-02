e354ab59c468a53320c2dde8d95ef97a5dfc029b
Ihor <80811711+Ingvarsvensen@users.noreply.github.com>
2023-11-02 19:05:55 +0100

    Update install_docker_yum.sh
    fix some issues

--- tools/install_docker_yum.sh
+++ tools/install_docker_yum.sh
@@ -4,7 +4,7 @@ dive="false"
 function="install"
 
 # Options
-. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/colors.sh) --
+source <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/colors.sh) --
 option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
 while test $# -gt 0; do
 	case "$1" in
