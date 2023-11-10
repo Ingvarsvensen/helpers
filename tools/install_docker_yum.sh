c4eac51f2cbdb07e9685a6ef7bd5c3b04dadf980
Ingvar <80811711+Ingvarsvensen@users.noreply.github.com>
2023-11-10 16:58:48 +0200

    Update install_docker_yum.sh

--- tools/install_docker_yum.sh
+++ tools/install_docker_yum.sh
@@ -1,53 +1,51 @@
 #!/bin/bash
-# Default variables
-dive="false"
-function="install"
+# Default action
+action="install"
 
-# Options
-. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/colors.sh) --
-option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
-while test $# -gt 0; do
-    case "$1" in
-    -h|--help)
-        # (snipped for brevity)
-        return 0 2>/dev/null; exit 0
-        ;;
-    -d|--dive)
-        dive="true"
-        shift
-        ;;
-    -u|--uninstall)
-        function="uninstall"
-        shift
-        ;;
-    *|--)
-        break
-        ;;
-    esac
-done
+# Check for -r parameter to uninstall
+if [ "$1" == "-r" ]; then
+    action="uninstall"
+fi
+
+# Download and source the colors script
+wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/colors.sh | bash
 
 # Functions
 install() {
-    cd
-    if ! docker --version; then
-        echo -e "${C_LGn}Docker installation...${RES}"
+    if ! command -v docker &> /dev/null; then
+        echo -e "${C_LGn}Docker is not installed. Installing Docker...${RES}"
         sudo yum update -y
         sudo yum install -y yum-utils device-mapper-persistent-data lvm2
         sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
         sudo yum install -y docker-ce docker-ce-cli containerd.io
-    fi  # Ensure this fi is here to close the above if block
-    if ! docker-compose --version; then
-        echo -e "${C_LGn}Docker Compose installation...${RES}"
+        echo -e "${C_LGn}Docker installed successfully.${RES}"
+    else
+        echo -e "${C_LGn}Docker is already installed.${RES}"
+    fi
+
+    if ! command -v docker-compose &> /dev/null; then
+        echo -e "${C_LGn}Docker Compose is not installed. Installing Docker Compose...${RES}"
         local docker_compose_version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name")
         sudo curl -L "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
-    fi  # Ensure this fi is here to close the above if block
-    if [ "$dive" = "true" ] && ! rpm -q dive; then
-        echo -e "${C_LGn}Dive installation...${RES}"
-        # (snipped for brevity, you may need to find or build a .rpm package for Dive)
-    fi  # Ensure this fi is here to close the above if block
+        echo -e "${C_LGn}Docker Compose installed successfully.${RES}"
+    else
+        echo -e "${C_LGn}Docker Compose is already installed.${RES}"
+    fi
 }
 
-# Actions
-$function
-echo -e "${C_LGn}Done!${RES}"
+uninstall() {
+    echo -e "${C_LGn}Uninstalling Docker and Docker Compose...${RES}"
+    sudo yum remove -y docker-ce docker-ce-cli containerd.io
+    sudo rm -f /usr/local/bin/docker-compose
+    echo -e "${C_LGn}Docker and Docker Compose have been uninstalled.${RES}"
+}
+
+# Execute action based on parameter
+if [ "$action" == "install" ]; then
+    install
+elif [ "$action" == "uninstall" ]; then
+    uninstall
+fi
+
+echo -e "${C_LGn}Operation completed.${RES}"
